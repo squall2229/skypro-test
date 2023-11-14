c891e70b5cdf067ee1b54c9e3d853ea5b7344194
pitbull <pitbull@Mashapro.local>
2023-11-14 21:22:35 +0100

    add plugin deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react-swc";
+import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
     base: "/skypro-test/",
     plugins: [
+        ghPages(),
         react(),
         svgr({
             include: "**/*.svg?react",
