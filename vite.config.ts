3246f2d400ba46b5f43bc7efb705653f0a1236c4
pitbull <pitbull@Mashapro.local>
2023-11-14 20:21:19 +0100

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      include: "**/*.svg?react",
-      exclude: "",
-    }),
-  ],
+    base: "/skypro-test/",
+    plugins: [
+        react(),
+        svgr({
+            include: "**/*.svg?react",
+            exclude: "",
+        }),
+    ],
 });
